8f9e7c0d3a56c7b64cee57f12319e22df75e1e42
Julien Wintz <julien.wintz@inria.fr>
2018-03-19 14:29:57 +0100

    Defer loading of magit-gitflow to keep insane load up time performance.

--- +vc.el
+++ +vc.el
@@ -1,7 +1,8 @@
 ;;; +vc.el --- description -*- lexical-binding: t; -*-
 
 (def-package! magit-gitflow
-  :config (add-hook 'magit-mode-hook 'turn-on-magit-gitflow))
+  :init (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+  :after magit)
 
 (provide '+vc)
 
