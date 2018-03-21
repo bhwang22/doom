f467f74a95a2a68d8cf4d48c030474064f1c5ca1
Julien Wintz <julien.wintz@inria.fr>
2018-03-21 21:49:58 +0100

    Fixed *which-key* popup location temporarily before it hits develop.

--- +popup.el
+++ +popup.el
@@ -1,8 +1,15 @@
 ;;; +popup.el --- description -*- lexical-binding: t; -*-
 
-(set! :popup "^ \\*which-key" '((size . +popup-shrink-to-fit) (vslot . 9999)))
-
-(advice-add #'display-buffer-in-side-window :override #'+popup-display-buffer)
+(setq which-key-popup-type 'custom
+      which-key-custom-popup-max-dimensions-function (lambda (_) (which-key--side-window-max-dimensions))
+      which-key-custom-hide-popup-function #'which-key--hide-buffer-side-window
+      which-key-custom-show-popup-function
+      (lambda (act-popup-dim)
+        (cl-letf (((symbol-function 'display-buffer-in-side-window)
+                   (lambda (buffer alist)
+                     (push '(vslot . -9999) alist)
+                     (+popup-display-buffer buffer alist))))
+          (which-key--show-buffer-side-window act-popup-dim))))
 
 (provide '+popup)
 
