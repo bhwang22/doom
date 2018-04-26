c769f2d00f8d9c27133a2fef5fd584b68936a182
Julien Wintz <julien.wintz@inria.fr>
2018-04-26 21:19:35 +0200

    Updating blog layer.

--- +blog.el
+++ +blog.el
@@ -3,10 +3,10 @@
 (def-package! org2jekyll
   :config
   (setq org2jekyll-blog-author "jwintz")
-  (setq org2jekyll-source-directory (expand-file-name "~/Sites/jwintz.github.io/_org"))
-  (setq org2jekyll-jekyll-directory (expand-file-name "~/Sites/jwintz.github.io/_site"))
-  (setq org2jekyll-jekyll-drafts-dir "_drafts")
-  (setq org2jekyll-jekyll-posts-dir "_posts")
+  (setq org2jekyll-source-directory (expand-file-name "~/Sites/jwintz.github.io/"))
+  (setq org2jekyll-jekyll-directory (expand-file-name "~/Sites/jwintz.github.io/"))
+  (setq org2jekyll-jekyll-drafts-dir "")
+  (setq org2jekyll-jekyll-posts-dir "_posts/")
   (setq org-publish-project-alist
     `(("default"
        :base-directory ,(org2jekyll-input-directory)
@@ -25,6 +25,19 @@
        :base-directory ,(org2jekyll-input-directory)
        :base-extension "org"
        :publishing-directory ,(org2jekyll-output-directory org2jekyll-jekyll-posts-dir)
+       :publishing-function org-html-publish-to-html
+       :headline-levels 4
+       :section-numbers nil
+       :with-toc nil
+       :html-preamble t
+       :recursive t
+       :make-index t
+       :html-extension "html"
+       :body-only t)
+      ("home"
+       :base-directory ,(org2jekyll-input-directory)
+       :base-extension "org"
+       :publishing-directory ,(org2jekyll-output-directory)
        :publishing-function org-html-publish-to-html
        :headline-levels 4
        :section-numbers nil
