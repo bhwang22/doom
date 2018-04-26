35ed72b7f18af3890b3e41bf1504a93fefde5fd9
Julien Wintz <julien.wintz@inria.fr>
2018-04-26 11:24:35 +0200

    Updating initilisation file. Get rid of persp-mode bindings.

--- +workspaces.el
+++ +workspaces.el
@@ -1,5 +1,5 @@
 ;;; +workspaces.el --- description -*- lexical-binding: t; -*-
 
-(setq persp-keymap-prefix (kbd "C-c w"))
+(setq persp-keymap-prefix (kbd ""))
 
 ;;; +workspaces.el ends here
--- init.el
+++ init.el
@@ -24,7 +24,7 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       evil-goggles      ; display visual hints when editing in evil
+      ;evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
        posframe          ; use child frames where possible (Emacs 26+ only)
@@ -36,36 +36,36 @@
        eshell            ; a consistent, cross-platform shell (WIP)
        gist              ; interacting with github gists
        imenu             ; an imenu sidebar and searchable code index
-       impatient-mode    ; show off code over HTTP
+      ;impatient-mode    ; show off code over HTTP
        make              ; run make tasks from Emacs
        magit             ;
        neotree           ; a project drawer, like NERDTree for vim
-       pdf               ; pdf enhancements
+      ;pdf               ; pdf enhancements
        rgb               ; creating color strings
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
-       tmux              ; an API for interacting with tmux
+      ;tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
 
        :lang
-       assembly          ; assembly for fun or debugging
+      ;assembly          ; assembly for fun or debugging
        cc                ; C/C++/Obj-C madness
       ;crystal           ; ruby at the speed of c
-       clojure           ; java with a lisp
-       csharp            ; unity, .NET, and mono shenanigans
+      ;clojure           ; java with a lisp
+      ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
-       erlang            ; an elegant language for a more civilized age
-       elixir            ; erlang done right
-       elm               ; care for a cup of TEA?
+      ;erlang            ; an elegant language for a more civilized age
+      ;elixir            ; erlang done right
+      ;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
-       ess               ; emacs speaks statistics
+      ;ess               ; emacs speaks statistics
       ;go                ; the hipster dialect
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       julia             ; a better, faster MATLAB
+      ;julia             ; a better, faster MATLAB
       (latex             ; writing papers in Emacs has never been so fun
        +latexmk
        +preview-pane)
-       lua               ; one-based indices? one-based indices
+      ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
@@ -79,7 +79,7 @@
        rest              ; Emacs as a REST client
        ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
       ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+      ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
        swift             ; who asked for emoji variables?
        typescript        ; javascript, but better
