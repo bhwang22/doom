e2379121f4a30da3559c7d2e3a712c15bf37b8db
Julien Wintz <julien.wintz@inria.fr>
2018-07-31 16:28:52 +0200

    Beautifying.

--- +ui.el
+++ +ui.el
@@ -1,20 +1,37 @@
 ;;; +ui.el --- description -*- lexical-binding: t; -*-
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Title bar setup
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (if (eq system-type 'darwin)
     (progn
       (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
       (add-to-list 'default-frame-alist '(ns-appearance . dark))))
 
-(add-to-list 'custom-theme-load-path (expand-file-name "+themes/" doom-private-dir))
-
 (setq ns-use-proxy-icon nil)
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Font setup
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq doom-font (font-spec :family "Source Code Variable" :size 10))
 (setq doom-variable-pitch-font (font-spec :family "Source Code Variable"))
 (setq doom-unicode-font (font-spec :family "Source Code Variable"))
 (setq doom-big-font (font-spec :family "Source Code Variable" :size 19))
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Theme setup
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq doom-theme 'doom-opera)
+
+(after! doom-themes
+  (custom-set-faces '(header-line ((t (:inherit neo-root-dir-face))))))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Whitespace mode setup
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (after! posframe
   (add-variable-watcher
@@ -32,15 +49,29 @@
 (advice-add #'company-box--make-frame :around #'doom*fix-whitespace-mode-in-childframes)
 (advice-add #'posframe--create-posframe :around #'doom*fix-whitespace-mode-in-childframes)
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Neotree setup
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (after! neotree
   (setq doom-neotree-enable-variable-pitch nil)
   (setq neo-autorefresh t))
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Resize window
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def-package! resize-window
   :commands  (resize-window))
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Page break lines
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def-package! page-break-lines
   :config (turn-on-page-break-lines-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide '+ui)
 
