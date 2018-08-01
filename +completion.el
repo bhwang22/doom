842a0905fdc8950d7fd67c52d99e9a78dbb6e619
Julien Wintz <julien.wintz@inria.fr>
2018-08-01 20:47:24 +0200

    Completion done right.

--- +completion.el
+++ +completion.el
@@ -5,13 +5,7 @@
   (setq company-echo-delay 0.0)
   (setq company-minimum-prefix-length 1)
   (setq company-tooltip-flip-when-above t)
-  (setq company-dabbrev-downcase nil)
-
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-elisp))
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-irony-c-headers))
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-cmake))
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-jedi)))
+  (setq company-dabbrev-downcase nil))
 
 (provide '+completion)
 
--- +lang|python.el
+++ +lang|python.el
@@ -3,6 +3,10 @@
 (after! conda
   (setq conda-anaconda-home "~/.conda"))
 
+(def-package! company-jedi
+  :config
+  (set-company-backend! 'python-mode 'company-jedi))
+
 (provide '+lang|python)
 
 ;;; +lang|python.el ends here
